c927dd5e3c216daf39b82b944e100e3ee87ce8c5
Justice Mubayiwa <j.mubayiwa@gmail.com>
2026-02-13 15:58:48 -0700

    Refine hero visuals, UX polish, and local Tailwind build setup

---
+++ config/tailwind.config.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+  content: ["./index.html", "./404.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "ui-sans-serif", "system-ui"],
+        display: ["Sora", "Manrope", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};
